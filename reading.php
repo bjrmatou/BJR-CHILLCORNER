e33ad1820ff29a7b88ce51b72c96ffa5276a49eb
bjrmatou <143578923+bjrmatou@users.noreply.github.com>
2023-09-20 20:03:47 +0200

    rognage de quelques images

--- reading.php
+++ reading.php
@@ -3,7 +3,7 @@
 $reading = [
     [
         'id' => 'Chnsw',
-        'image' => '<img src="img/jujutsu-kaisen.jpg" alt="JJK card cover">',
+        'image' => '<img src="img/chainsaw-man.png" alt="Chainsaw man card cover">',
         'titre' => 'Chainsaw Man',
         'description' => 'Manga - TOME 12/ONGOING',
         'note' => '★★★★★'
@@ -11,7 +11,7 @@ $reading = [
 
         [
             'id' => 'Brsrk',
-            'image' => '<img src="img/jujutsu-kaisen.jpg" alt="JJK card cover">',
+            'image' => '<img src="img/berserk.png" alt="Berserk card cover">',
             'titre' => 'Berserk',
             'description' => 'MANGA - TOME 41/41',
             'note' => '★★★★★'
@@ -19,7 +19,7 @@ $reading = [
 
             [
                 'id' => 'Snk',
-                'image' => '<img src="img/jujutsu-kaisen.jpg" alt="JJK card cover">',
+                'image' => '<img src="img/aot.jpg" alt="SNK card cover">',
                 'titre' => 'Shingeki no Kyojin',
                 'description' => 'MANGA - TOME 34/34',
                 'note' => '★★★★★'
