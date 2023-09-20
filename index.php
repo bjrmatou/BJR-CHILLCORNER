bb1d71f516d8d2baa00afa5d402a34679392b4c8
bjrmatou <143578923+bjrmatou@users.noreply.github.com>
2023-09-20 06:00:15 +0200

    création du modèle des vignettes

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <meta name="description" content="bjrmatou's digital shelf/messy dump/personal netflix but you can't stream anything, I just give my unsollicited advice on random things I consume during my free time... Anyway - call it whatever you want. This is a non-exhaustive list of the things I like.">
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>BJRCHILLCORNER</title>
 </head>
 <body>
@@ -14,8 +17,47 @@
 
 
         <div class="main-container">
-        TEST
-        <div class="test1">TEST 2</div>
+
+
+
+
+            <div class="categ-container">
+                <h4>WATCHING</h4>
+                <article class="bjr-card">                
+                    <div class="bjr-card-imgzone">
+                        <img src="img/jujutsu-kaisen.jpg">
+                    </div>
+                        <h5>TITRE</h5>
+                        <p class="bjr-card-desc">
+                            description 
+                        </p>
+                        <div class="bjr-card-note">
+                            <i class="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                                    <i class="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                                    <i class="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                                    <i class="fa-xs fa-solid fa-star" aria-hidden="true"></i>
+                                    <i class="fa-xs fa-solid fa-star neutral-star" aria-hidden="true"></i>
+                        </div>
+                </article>
+            </div>
+
+
+
+
+            <div class="categ-container">
+                <h4>READING</h4>
+            </div>
+
+
+
+
+            <div class="categ-container">
+                <h4>PLAYING</h4>
+            </div>
+
+
+
+
         </div>
         
         
