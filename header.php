c0173332c0cd6c401d87033adfe5e09a50c0c0ed
bjrmatou <143578923+bjrmatou@users.noreply.github.com>
2023-09-20 02:37:48 +0200

    normalize css + finalisation header & footer

--- header.php
+++ header.php
@@ -1,6 +1,10 @@
 <div class="sitename">
-    <h1>BJRMATOU'S</h1>
-    <br><h2>LIBRARY</h2>
+    <div>
+        <h1>BJRMATOU'S</h1>
+    </div>
+    <div>
+        <br><h2>LIBRARY</h2>
+    </div>
 </div>
 <div class="sitelogo">
     <img src="img/logo_gengar.png" alt="Logo BJRCHILLCORNER - Gengar, my fav pokemon!" id="logo">
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
     <meta charset="utf-8">
     <meta name="description" content="bjrmatou's digital shelf/messy dump/personal netflix but you can't stream anything, I just give my unsollicited advice on random things I consume during my free time... Anyway - call it whatever you want. This is a non-exhaustive list of the things I like.">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
     <title>BJRCHILLCORNER</title>
 </head>
 <body>
@@ -10,8 +11,13 @@
         <?php include('header.php'); ?>
     </header>
     <main>
+
+
+        <div class="main-container">
         TEST
         <div class="test1">TEST 2</div>
+        </div>
+        
         
 
 
