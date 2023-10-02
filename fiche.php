796aa7458cbf8090f7dd3bbe24a46a7763dc0c1a
bjrmatou <143578923+bjrmatou@users.noreply.github.com>
2023-10-02 02:31:13 +0200

    contenu fiche et fichier stylefiche

--- fiche.php
+++ fiche.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="description" content="bjrmatou's digital shelf/messy dump/personal netflix but you can't stream anything, I just give my unsollicited advice on random things I consume during my free time... Anyway - call it whatever you want. This is a non-exhaustive list of the things I like.">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/stylefiche.css">
     <link rel="stylesheet" href="normalize.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
         integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
@@ -19,6 +20,33 @@
     </header>
 
     <main>
+        <div class="fiche-maincont">
+            <div class="cont-inline">
+
+                <div class="fiche-img">
+                    <img src="img/jujutsu-kaisen.jpg" alt="JJK card cover">
+                </div>
+
+                <div class="fiche-contenu">
+
+                    <h3>Titre</h3>
+
+                    <div class="fiche-rating">
+                        note
+                    </div>
+
+                    <p>
+                        resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume resume
+                    </p>
+
+                    <div class="fiche-avis">
+                        avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis avis 
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
 
 
 
